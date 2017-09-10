0ecea0ec4dfcf6032aa844988891ff0982f393f0
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-09-10 21:21:07 +0100

    layout styled

--- public/picture_select_view.js
+++ public/picture_select_view.js
@@ -4,14 +4,26 @@ var PictureView = function(nasaData) {
 }
 
 PictureView.prototype.populate = function() {
-    var div = document.getElementById('rover-gallery');
-    console.log(this.nasaData);
-    console.log(this.nasaData.album);
-    console.log(this.nasaData.album.photos);
+    var galleryContainer = document.getElementById('gallery-container');
+    while(galleryContainer.firstChild) {
+        galleryContainer.removeChild(galleryContainer.firstChild);
+      }
     var pictures = this.nasaData.album.photos;
     pictures.forEach(function(picture, index) {
+        var responsiveDiv = document.createElement('div');
+        galleryContainer.appendChild(responsiveDiv);
+        responsiveDiv.setAttribute('class', 'responsive');
+        var galleryDiv = document.createElement('div');
+        responsiveDiv.appendChild(galleryDiv);
+        galleryDiv.setAttribute('class', 'gallery');
+        galleryDiv.setAttribute('id', 'rover-gallery');
+        var link = document.createElement('a');
+        galleryDiv.appendChild(link);
+        link.setAttribute('target', '_blank');
+        link.setAttribute('href', picture.img_src);
         var img = document.createElement('IMG');
-        img.setAttribute('src', picture.img_src)
-        div.appendChild(img);   
+        img.setAttribute('src', picture.img_src);
+        img.setAttribute('class', 'mars-image')     
+        link.appendChild(img);   
     });
 }
